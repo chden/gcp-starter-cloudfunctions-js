58908d56ba93dff18b936c26ad6b7af1dba5278a
chden <3658166+chden@users.noreply.github.com>
2020-07-29 18:44:58 +0200

    Initial logging with logging api AND winston

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,28 @@
+'use strict';
+
+const winston = require('winston');
+// const expressWinston = require('winston-express-middleware');
+const {LoggingWinston} = require('@google-cloud/logging-winston');
+
+const loggingWinston = new LoggingWinston();
+
+const logger = winston.createLogger({
+    level: 'info',
+    transports: [
+        new winston.transports.Console(),
+        // Add Stackdriver Logging
+        loggingWinston,
+    ],
+    msg: 'HTTP {{req.method}} {{req.url}}',
+});
+
+
 const cors = require('cors')({
     origin: true,
 });
 
 exports.main = (req, res) => {
+    logger.info('TEST2 WINSTON');
     cors(req, res, () => {});
     res.send('Hello, World!');
 };
